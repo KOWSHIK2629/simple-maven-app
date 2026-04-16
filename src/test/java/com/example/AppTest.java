7f53d7d24fa778f37738529b6393208095dbd148
KOWSHIK2629 <murugesankowshik29@gmail.com>
2026-04-16 11:32:37 +0530

    Add Jenkins pipeline for build and test
    Added a Jenkins pipeline script for CI/CD.

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -8,3 +8,28 @@ App app = new App();
 assertEquals(5, app.add(2, 3));
 }
 }
+
+
+
+
+scriptt : 
+
+pipeline {
+agent any
+tools {
+maven 'M3'
+}
+stages {
+stage('Checkout Git') {
+steps {
+git branch: 'main',
+url: 'https://github.com/balaganeshn/simple-maven-app'
+}
+}
+stage('Build and Test') {
+steps {
+bat 'mvn clean test'
+}
+}
+}
+}
