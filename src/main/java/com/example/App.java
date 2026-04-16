db0fa1e7e5a31b0d024f56e258b584f987781207
KOWSHIK2629 <murugesankowshik29@gmail.com>
2026-04-16 10:24:43 +0530

    Add App class with main method and sum calculation

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven");
+
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum = " + (a + b));
+    }
+}
